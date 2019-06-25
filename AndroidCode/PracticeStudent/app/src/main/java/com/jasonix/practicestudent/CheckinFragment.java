8a8036f77ed13ea61dd96086e90055afc8e1c7f2
常佳鑫 <ccjjxx99@qq.com>
2019-06-25 18:30:07 +0800

    创建安卓基本框架

---
+++ AndroidCode/PracticeStudent/app/src/main/java/com/jasonix/practicestudent/CheckinFragment.java
@@ -0,0 +1,21 @@
+package com.jasonix.practicestudent;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CheckinFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_checkin,container,false);
+    }
+}
--- AndroidCode/PracticeStudent/app/src/main/java/com/jasonix/practicestudent/MainActivity.java
+++ AndroidCode/PracticeStudent/app/src/main/java/com/jasonix/practicestudent/MainActivity.java
@@ -2,13 +2,19 @@ package com.jasonix.practicestudent;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    private CheckinFragment checkinFragment;
+    private DiaryFragment diaryFragment;
+    private MineFragment mineFragment;
+    private Fragment[] fragments;
+    private int lastShowFragment = 0;   //表示最后一个显示的Fragment
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,13 +23,22 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    if (lastShowFragment != 0) {
+                        switchFragment(lastShowFragment, 0);
+                        lastShowFragment = 0;
+                    }
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    if (lastShowFragment != 1) {
+                        switchFragment(lastShowFragment, 1);
+                        lastShowFragment = 1;
+                    }
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    if (lastShowFragment != 2) {
+                        switchFragment(lastShowFragment, 2);
+                        lastShowFragment = 2;
+                    }
                     return true;
             }
             return false;
@@ -35,8 +50,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        checkinFragment = new CheckinFragment();
+        diaryFragment = new DiaryFragment();
+        mineFragment = new MineFragment();
+        fragments = new Fragment[]{checkinFragment, diaryFragment, mineFragment};
+        //lastShowFragment = 0;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, checkinFragment)
+                .show(checkinFragment)
+                .commit();
+    }
+
+    public void switchFragment(int lastIndex, int index) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastIndex]);
+        if (!fragments[index].isAdded()) {
+            transaction.add(R.id.container, fragments[index]);
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
     }
 
 }
