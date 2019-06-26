b3d23f34aaa12cf9c051a38c4b419731055c185d
DaiDaotang <1025563447@qq.com>
2019-06-26 08:46:22 +0800

    连接数据库库

---
+++ BackCode/src/com/DBConn.java
@@ -0,0 +1,71 @@
+package com;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DBConn {
+    private static String dbDriver;
+    private static String url;
+    private static String user;
+    private static String password;
+
+    //静态变量初始化
+    static {
+        Connection con;
+        //jdbc驱动
+        dbDriver="com.mysql.cj.jdbc.Driver";
+        url="jdbc:mysql://localhost:8080/demo?&useSSL=false&serverTimezone=UTC";
+        user="root";
+        password=" ";
+        try {
+            //注册JDBC驱动程序
+            Class.forName(dbDriver);
+            //建立连接
+            con = DriverManager.getConnection(url, user, password);
+            if (!con.isClosed()) {
+                System.out.println("数据库连接成功");
+            }
+            con.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("数据库驱动没有安装");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("数据库连接失败");
+        }
+    }
+    public static Connection getConnection(){
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("链接信息：" + connection);
+            return connection;
+        }catch (SQLException e){
+            System.err.println("链接信息异常");
+            return null;
+        }
+    }
+    public static void closeConn(Connection conn){
+        if(conn != null){
+            try{
+                conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            conn = null;
+        }
+    }
+    public static void rollback(Connection conn){
+        if(conn != null){
+            try{
+                conn.rollback();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        conn = null;
+    }
+}
