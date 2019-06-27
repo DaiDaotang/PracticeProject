e390de2825466b6c3265e553d11b6c6bca3cb30a
DaiDaotang <1025563447@qq.com>
2019-06-27 11:11:13 +0800

    login

--- BackCode/src/com/servlet/LoginServlet.java
+++ BackCode/src/com/servlet/LoginServlet.java
@@ -1,19 +1,60 @@
 package com.servlet;
 
+import com.bean.CompanyBean;
+import com.bean.Loginbean;
+import com.bean.RequestBean;
+import com.bean.ResponseBean;
+import com.dao.CompanySignupDao;
+import com.dao.LoginDao;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
 
 @WebServlet(name = "LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        BufferedReader reader = request.getReader();
+        String content = reader.readLine();
+        Gson gson = new Gson();
+        Type requestType = new TypeToken<RequestBean<Loginbean>>(){}.getType();
+        RequestBean<Loginbean> reqBean = gson.fromJson(content,requestType);
+        ResponseBean<Loginbean> resBean = new ResponseBean<>();
+        try{
+            LoginDao dao = new LoginDao();
+            Loginbean loginbean = dao.checkIn(reqBean);
+            if (loginbean == null){
+                resBean.setReqId(reqBean.getReqId());
+                resBean.setSuccess(false);
+            }
+            else {
+                resBean.setReqId(reqBean.getReqId());
+                resBean.setSuccess(true);
+                resBean.setResData(loginbean);
+            }
+            //识别ResponseBean<LoginBean>类的结构
+            Type respType = new TypeToken<ResponseBean<Loginbean>>(){}.getType();
+            //通过toJson方法将对象转化为json格式的字符串
+            out.print(gson.toJson(resBean,respType));
+        }catch (Exception e){
+            out.print(e.toString());
+        }
+        out.flush();
+        out.close();
     }
 }
