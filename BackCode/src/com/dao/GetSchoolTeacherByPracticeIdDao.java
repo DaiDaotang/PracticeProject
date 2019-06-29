882525dc384a0919e48dd0c655b911b5db0fd278
常佳鑫 <ccjjxx99@qq.com>
2019-06-29 20:09:49 +0800

    获取实训的学校老师

---
+++ BackCode/src/com/dao/GetSchoolTeacherByPracticeIdDao.java
@@ -0,0 +1,41 @@
+package com.dao;
+
+import com.DBConn;
+import com.bean.SchoolTeacherBean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class GetSchoolTeacherByPracticeIdDao {
+    public ArrayList<SchoolTeacherBean> GetSchoolTeacher(int practiceId)
+    {
+        ArrayList<SchoolTeacherBean> teacherBeans = new ArrayList<>();
+        Connection conn = DBConn.getConnection();
+        PreparedStatement state;
+        try{
+            conn.setAutoCommit(false);
+            String sql ="SELECT schoolteacherId,schoolteacherName,schoolTeacherSex FROM pracstrelation,schoolteacher WHERE practice id = ? AND pracstrelation.schoolTeacherId = schoolteacher.schoolTeacherId";
+            state = conn.prepareStatement(sql);
+            state.setInt(1,practiceId);
+            ResultSet rs = state.executeQuery();
+            while (rs.next()){
+                SchoolTeacherBean teacherBean = new SchoolTeacherBean();
+                teacherBean.setId(rs.getInt(1));
+                teacherBean.setName(rs.getString(2));
+                teacherBean.setSex(rs.getString(3));
+                teacherBeans.add(teacherBean);
+            }
+            conn.commit();
+            return teacherBeans;
+        }catch (SQLException e){
+            e.printStackTrace();
+            DBConn.rollback(conn);
+            return null;
+        }finally {
+            DBConn.closeConn(conn);
+        }
+    }
+}
---
+++ BackCode/src/com/servlet/GetSchoolTeacherByPracticeIdServlet.java
@@ -0,0 +1,58 @@
+package com.servlet;
+
+import com.bean.RequestBean;
+import com.bean.ResponseBean;
+import com.bean.SchoolTeacherBean;
+import com.dao.GetSchoolTeacherByPracticeIdDao;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class GetSchoolTeacherByPracticeIdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        BufferedReader reader = request.getReader();
+        String content = reader.readLine();
+        Gson gson = new GsonBuilder().setDateFormat("yyyy").create();
+        Type requestType = new TypeToken<RequestBean<Integer>>(){}.getType();
+        RequestBean<Integer> reqBean = gson.fromJson(content,requestType);
+        ResponseBean<ArrayList<SchoolTeacherBean>> resBean = new ResponseBean<>();
+        try{
+            GetSchoolTeacherByPracticeIdDao dao = new GetSchoolTeacherByPracticeIdDao();
+            ArrayList<SchoolTeacherBean> arrayList = new ArrayList<>();
+            arrayList = dao.GetSchoolTeacher(reqBean.getReqParam());
+            if (arrayList == null){
+                resBean.setResId(reqBean.getReqId());
+                resBean.setSuccess(false);
+            }
+            else {
+                resBean.setResId(reqBean.getReqId());
+                resBean.setSuccess(true);
+                resBean.setResData(arrayList);
+            }
+            Type respType = new TypeToken<ResponseBean<ArrayList<SchoolTeacherBean>>>(){}.getType();
+            String s = gson.toJson(resBean,respType);
+            out.print(s);
+        }catch (Exception e){
+            out.print(e.toString());
+        }
+        out.flush();
+        out.close();
+    }
+}
